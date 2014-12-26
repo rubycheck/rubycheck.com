baf6a3a9a92b07caded7afa7436946c0d3c662e9
Adam Niedzielski <adamsunday@gmail.com>
2014-12-26 16:20:18 +0100

    Temporarily change root directory to /rubycheck.com before setting up DNS

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/rubycheck.com/"
+http_images_path = "/rubycheck.com/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/rubycheck.com/fonts"
+css_dir = "public/rubycheck.com/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
